fe509c132155428b4301f09b54368131492e1226
alexrodriguezalejo <alexrodriguezalejo@gmail.com>
2023-05-15 00:43:40 -0300

    js en botones

--- js/js.js
+++ js/js.js
@@ -6,3 +6,13 @@ function confirmacionSuscribirse() {
         return false;
     }
 }
+
+function confirmacion() {
+    var respuesta = confirm("Estas seguro que desea enviar esta informacion?");
+    if (respuesta == true) {
+        return true;   
+    } else {
+        return false;
+    }
+}
+
